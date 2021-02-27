8b1b95a09ed9e8ec03f3fad946dde841dad74aab
IwailerI <andrypirk@gmail.com>
2021-02-27 12:11:47 +0200

    Messing around, editing just for testing idk why commiting

--- materials/helloworld.go
+++ materials/helloworld.go
@@ -1,30 +1,9 @@
 package main
 
-import (
-	"fmt"
-)
+import "github.com/nsf/termbox-go"
 
 var done int
 
 func main() {
-	c := make(chan bool)
-	go print("AA", c)
-	go print("bb", c)
-	go print("CC", c)
-	<-c
-	<-c
-	<-c
-	fmt.Println(done)
-
-}
-
-// is goroutine
-func print(msg string, inp chan bool) {
-	for i := 0; i < 100; i++ {
-		fmt.Println(msg)
-	}
-	done++
-	fmt.Print(1, msg)
-	inp <- true
-	fmt.Println(msg)
+	termbox.Init()
 }
